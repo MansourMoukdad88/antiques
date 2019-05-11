3c96740a7f91b2fa4e6b5798d7acab01e2062066
MansourMoukdad88 <m_sy@outlook.com>
2019-05-11 22:38:31 +0300

    Refactor Callbacks with Async + Await

--- seeds.js
+++ seeds.js
@@ -1,66 +1,92 @@
 const mongoose = require("mongoose");
 const Antique = require("./models/antique");
 const Comment = require("./models/comment");
-let data = [
+let  seeds = [
+
   {
-    name: "Amman Antiques",
-    image:
-      "https://media.thatsweetgift.com/wp-content/uploads/2017/10/antiques.png",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+      name: "Cloud's Rest", 
+
+      image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+
   },
+
   {
-    name: "Aqaba Antiques",
-    image:
-      "https://media.thatsweetgift.com/wp-content/uploads/2017/10/antiques.png",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+      name: "Desert Mesa", 
+
+      image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+
   },
+
   {
-    name: "Damascus Antiques",
-    image:
-      "https://www.thoughtco.com/thmb/izTzjufWQzSYBSsgbgMpOEkLiDE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-488004795-5c70260546e0fb0001b681d6.jpg",
-    description:
-      "TLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+      name: "Canyon Floor", 
+
+      image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+
   }
+
 ];
 
-function seedDb() {
-  // remove all antiques from DB
-  Antique.remove({}, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Removed Antiques!!");
-      data.forEach(seed => {
-        Antique.create(seed, (err, antique) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("Data Created");
-            //create comments
-            Comment.create(
+
+
+async function seedDB(){
+
+  try {
+
+      await Campground.remove({});
+
+      console.log('Campgrounds removed');
+
+      await Comment.remove({});
+
+      console.log('Comments removed');
+
+
+
+      for(const seed of seeds) {
+
+          let campground = await Campground.create(seed);
+
+          console.log('Campground created');
+
+          let comment = await Comment.create(
+
               {
-                text: "This is a very old antique, 1200 B.C",
-                author: "Arabian Guy"
-              },
-              (err, comment) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  antique.comments.push(comment);
-                  antique.save();
-                  console.log("Comment Has Created");
-                }
+
+                  text: 'This place is great, but I wish there was internet',
+
+                  author: 'Homer'
+
               }
-            );
-          }
-        });
-      });
-    }
-  });
-  // add antiques
-  // add comments
+
+          )
+
+          console.log('Comment created');
+
+          campground.comments.push(comment);
+
+          campground.save();
+
+          console.log('Comment added to campground');
+
+      }
+
+  } catch(err) {
+
+      console.log(err);
+
+  }
+
 }
 
-module.exports = seedDb;
+
+
+module.exports = seedDB;
